a6db587d82d739c315c56fd3f9d3ea01dc1f2264
Javed <khans.jak@gmail.com>
2023-04-05 11:42:59 +0530

    This is first commit

---
+++ accessapi.py
@@ -0,0 +1,9 @@
+secret_key='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'
+import openai
+openai.api_key=secret_key
+prompt = 'tell me slogan for a security company'
+
+output = openai.Completion.create(
+    model = 'text-davinci-003',
+    prompt = 
+)
