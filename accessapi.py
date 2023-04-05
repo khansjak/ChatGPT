568e719b8c66802eae9395a56964721fe7efc07b
Javed <khans.jak@gmail.com>
2023-04-05 12:16:45 +0530

    commit

--- accessapi.py
+++ accessapi.py
@@ -1,12 +1,13 @@
-secret_key='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'
 import openai
-openai.api_key=secret_key
 prompt = 'tell me slogan for a security company'
+secret_key = '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'
+openai.api_key = secret_key
+
 
 output = openai.Completion.create(
-    model = 'text-davinci-003',
-    prompt = prompt,
+    model='text-davinci-003',
+    prompt=prompt,
     max_tokens=200,
-    temperature =0
+    temperature=0
 )
 print(output)
